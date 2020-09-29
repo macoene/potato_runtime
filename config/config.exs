78fe25b34b33af18ab885f31443adc30aaf35f05
Maxime Coene <macoene@vub.be>
2020-09-29 19:52:59 +0200

    Fixes

--- config/config.exs
+++ config/config.exs
@@ -10,17 +10,19 @@ use Mix.Config
 
 # You can configure your application as:
 #
-#     config :potato_example, key: :value
+#     config :potato, key: :value
 #
 # and access this configuration in your application as:
 #
-#     Application.get_env(:potato_example, :key)
+#     Application.get_env(:potato, :key)
 #
 # You can also configure a 3rd-party app:
 #
 #     config :logger, level: :info
 #
-
+config :logger, :console,
+  format: "\n$time $levelpad$message\n",
+  metadata: [:user_id, :module]
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
