cf40f8967fb9274ce6ba953fcb1c792822c1a9ae
Maxime Coene <macoene@vub.be>
2021-04-19 14:48:05 +0200

    Added Santander: Smartphone (for monitoring device)

---
+++ examples/lib/santander/smartphone(forMonitorDevice).ex
@@ -0,0 +1,60 @@
+defmodule Potato.Santander.SmartphoneMD do
+    alias Observables.Obs
+    alias Observables.Subject
+    alias Potato.Network.Observables, as: Net
+    require Logger
+    use Potato.DSL
+  
+    @doc """
+    User smartphone that wants to access data (for example air
+    pollution status or noise) from an environment monitoring 
+    device.
+    """
+  
+    def init() do
+      # Node descriptor
+      nd = %{
+        hardware: :smartphone,
+        type: :smartphoneMD,
+        name: "smartphone (for monitoring device",
+        uuid: Node.self()
+      }
+  
+      Potato.Network.Meta.set_local_nd(nd)
+    end
+  
+    def run() do
+      init()
+  
+      joins = 
+        Net.network()
+        |> Obs.filter(&match?({:join, _}, &1))
+        |> Obs.filter(fn {:join, v} ->
+          v.type == :monitorEnvironment
+        end)
+        |> Obs.map(&elem(&1, 1))
+        |> Obs.each(fn nd ->
+          Logger.debug("Joined Key Reader: #{inspect(nd)}")
+        end)
+
+      {sink, sink_to_pass} = create_remote_variable("smartphone sink")
+  
+      prog = program [after_life: :kill, leasing_time: 1500, sinks: [{sink, sink_to_pass}]] do
+          Obs.range(1, :infinity)
+          |> Obs.map(fn _ ->
+            Potato.Santander.MonitoringDevice.air_pollution()
+          end, true)
+          |> Obs.map(fn v ->
+            Subject.next(use_sink(sink_to_pass), v)
+          end)
+      end
+  
+      sink
+      |> Obs.filter(fn {{t, _}, _} -> t == :monitorEnvironment end)
+      |> Obs.each(fn {_, v} -> 
+        IO.inspect(v, label: "Receiven AQI value")
+      end)
+  
+      send_program(prog, joins)
+    end
+  end
